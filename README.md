b8fd4d4b3b90963c83b99ca0e0cf9a2a37f4d992
WDRETZER <walter_retzer@yahoo.com.br>
2022-04-26 00:44:02 -0300

    Melhoria das Imagens da Tela de Informações dos Planetas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NasaProjetoIntegrador
-Aplicativo Desenvolvido com o Tema NASA para a prática da Linguagem Kotlin
+Aplicativo Desenvolvido para a prática da Linguagem Kotlin com o Tema NASA voltado ao público infantil com o intuito de levar um pouco sobre astronomia para as crianças.
+
+Consumindo API da NASA para exibir imagens do Sistema Solar utilizando Retrofit, Banco de Dados com a biblioteca ROOM, Shared Preferences para armazenar o Nome do Astronauta e Avatar da Imagem do Perfil do Usuário.
--- app/src/main/java/com/wdretzer/nasaprojetointegrador/detalheplaneta/DetalhePlaneta.kt
+++ app/src/main/java/com/wdretzer/nasaprojetointegrador/detalheplaneta/DetalhePlaneta.kt
@@ -1,7 +1,9 @@
 package com.wdretzer.nasaprojetointegrador.detalheplaneta
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.wdretzer.nasaprojetointegrador.R
 
 class DetalhePlaneta : AppCompatActivity() {
@@ -11,5 +13,11 @@ class DetalhePlaneta : AppCompatActivity() {
 
         // Desabilita a Action Bar que exibe o nome do Projeto:
         getSupportActionBar()?.hide()
+
+        val sistemaSolar = findViewById<ImageView>(R.id.img_detalhe_planeta)
+        Glide.with(this)
+            .asGif()
+            .load("https://i.pinimg.com/originals/bd/6b/1a/bd6b1a3f7676b2a4b0fbead6668e2773.gif")
+            .into(sistemaSolar)
     }
 }
